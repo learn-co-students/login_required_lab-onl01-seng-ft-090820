c31a28c3a16bf08584cd494a3981a8809e6f54d3
doosemavis <moosedavis2011@gmail.com>
2020-11-22 16:26:03 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
-  get '/show', to: "sessions#show"
+  get '/show', to: "secrets#show"
 
 end

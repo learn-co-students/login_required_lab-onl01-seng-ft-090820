32e1a5031d8684f00e85374de649eeba482c8a42
adarvin14 <amdarvin14@gmail.com>
2020-11-30 15:20:26 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,16 @@
+class SecretsController < ApplicationController
+    before_action do
+        redirect_to '/login' unless session[:name]
+      end
+
+    def show
+
+    end
+
+    private
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
+
+end
